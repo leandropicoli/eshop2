0ce01d99cde1c72f63954feb17a7c749006de422
Leandro Picoli <leandropicoli@live.com>
2020-10-21 20:41:19 -0300

    Created Navbar

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 
 @NgModule({
@@ -14,6 +15,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    NavbarComponent
+  ]
 })
-export class HomePageModule {}
+export class HomePageModule { }
